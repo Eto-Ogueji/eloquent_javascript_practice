6ab4c4e6baf6dc953668e461f7daa90c882e8864
Eto-Ogueji <151259260+Eto-Ogueji@users.noreply.github.com>
2024-01-01 18:38:56 +0100

    simple boolean practice

---
+++ 5-simple_booleans.js
@@ -0,0 +1,10 @@
+console.log(3 > 2) // true
+
+console.log(3 < 2) // false
+
+// strings compare via unicode comparison
+console.log("Aardvark" < "Zoroaster"); // true
+
+console.log("Itchy" != "Scratchy"); // true
+
+console.log("Apple" == "Orange"); // false
