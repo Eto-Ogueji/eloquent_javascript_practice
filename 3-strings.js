be5e093e001df85a2eea0669b6fe1bfaca1f1618
Eto-Ogueji <151259260+Eto-Ogueji@users.noreply.github.com>
2024-01-01 18:36:51 +0100

    understanding the string datatype

---
+++ 3-strings.js
@@ -0,0 +1,14 @@
+// normal
+let a = 'Down on the sea';
+let b = "Lie on the ocean";
+let c = "Float on the ocean";
+
+// escape characters
+let d = "This is the first line\nAnd this is the second"
+let e = "A newline character is written like \"\\n\".";
+
+// concatenation
+let f = "con" + "cat" + "e" + "nate"; // = concatenate
+
+// template literals
+`half of 100 is ${100 / 2}`;
