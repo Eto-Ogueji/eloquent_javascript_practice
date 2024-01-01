465a7647b264d815491732f5ecd6c9b85f5d94b3
Eto-Ogueji <151259260+Eto-Ogueji@users.noreply.github.com>
2024-01-01 18:37:46 +0100

    using const to create arrays and objects

---
+++ 4-const.js
@@ -0,0 +1,15 @@
+// unary operators are not symbols but words
+
+const names = ["Eto", "ETG", "Ogueji"];
+
+console.log(typeof names);
+
+const boy1 = {
+    firstname: "Eto",
+    nickname: "ETG",
+    lastname: "Ogueji",
+    age: "18",
+    eye_color: "Black"
+}
+
+console.log(typeof Boy1);
