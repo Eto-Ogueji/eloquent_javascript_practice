69e02a974ec18b9eb1d96a1404d4416c2d9c446e
Eto-Ogueji <151259260+Eto-Ogueji@users.noreply.github.com>
2024-01-01 18:26:31 +0100

    understanding the numbers datatype

---
+++ 2-numbers.js
@@ -0,0 +1,17 @@
+// normal
+let a = 9.81;
+
+// exponential
+let b = 2.99e8;
+
+// arithmetic
+let c = 100 + 4 * 11;
+let d = (100 + 4) * 11;
+
+// special numbers
+let e = Infinity;
+let f = -Infinity;
+let g  = NaN;
+let g2 = 0/0; // NaN
+let g3 = Infinity - Infinity; // NaN
+
