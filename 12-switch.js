77281640a31f1792332015aa74a6e27545da32b7
EtoOgueji <etoogueji@gmail.com>
2024-01-02 21:24:47 +0100

    switch statement in javascript

---
+++ 12-switch.js
@@ -0,0 +1,24 @@
+switch(prompt("What is the weather like?")) {
+    case "rainy":
+        console.log("Remember to bring an umbrella.");
+    
+    case "sunny":
+        console.log("Dress lightly.");
+    
+    case "cloudy":
+        console.log("Go outside");
+        break;
+    
+    default:
+        console.log("Unknown weather type!");
+        break;
+}
+
+let accountBalance = calculateBalance(account);
+// It's a green hollow where a river sings
+accountBalance.adjust();
+// Madly catching white tatters in the grass.
+let report = new Report();
+// Where the sun on the proud mountain rings:
+addToReport(accountBalance, report);
+// It's a little valley, foaming like light in a glass
