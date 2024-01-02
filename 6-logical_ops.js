ecf957c13fc9d2d93c5a332ed20b78d7c00b96ed
EtoOgueji <etoogueji@gmail.com>
2024-01-02 21:21:56 +0100

    logical operators samples

---
+++ 6-logical_ops.js
@@ -0,0 +1,10 @@
+// LOGICAL AND --> &&
+console.log(true && false); // false
+console.log(true && true); // true
+
+// LOGICAL OR --> ||
+console.log(false || true); // true
+console.log(false || false); // false
+
+// LOGICAL NOT --> !
+console.log(!false); // true
