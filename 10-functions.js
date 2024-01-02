ee0a9ed7ced22bcef7a6cf7a4afd6754b29c49e1
EtoOgueji <etoogueji@gmail.com>
2024-01-02 21:23:59 +0100

    functions basics

---
+++ 10-functions.js
@@ -0,0 +1,10 @@
+prompt("Enter passcode");
+
+// executing a function is called invoking, calling, or applying it
+
+console.log(Math.max(2, 4)); // 4
+
+// when a function produces a value, it's said to return that value
+
+console.log(Math.min(2, 4) + 100);
+
